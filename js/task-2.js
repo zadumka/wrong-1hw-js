1c5013a307cbd299de83dd7a690f04aa3bf3e408
zadumka <90329284+zadumka@users.noreply.github.com>
2025-02-14 14:20:13 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getShippingMessage(country, price, deliveryFee) {
-  var totalPrice = price + deliveryFee;
+   totalPrice = price + deliveryFee;
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
