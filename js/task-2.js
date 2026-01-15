5b9e3c979a04c0a86efc6693aea928eaa34504d8
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-15 11:45:02 +0200

    Replace getShippingMessage with makeTransaction function

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function getShippingMessage(country, price, deliveryFee) {
-   totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+function makeTransaction(quantity, pricePerDroid) {
+return `You ordered ${quantity} droids worth ${quantity + pricePerDroid} credits!`;
 }
 
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
