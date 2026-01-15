b63d6a13639cedc6f9cc3963fe25b8f2bf4bcffc
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-15 11:54:09 +0200

    Fix transaction calculation in makeTransaction function

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-return `You ordered ${quantity} droids worth ${quantity + pricePerDroid} credits!`;
-}
-
+  console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
+};
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
